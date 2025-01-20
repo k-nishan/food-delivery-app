6f1a061018ee1dd908d2b30bb1eedfd14a13fa10
nishanthan-personal <nishakanaga0708@gmail.com>
2025-01-20 19:58:32 +0530

    selected item color change

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -3,6 +3,7 @@ import "./ExploreMenu.css";
 import { menu_list } from "../../assets/assets";
 
 const ExploreMenu = ({ category, setCategory }) => {
+
   return (
     <div className='explore-menu' id='explore-menu'>
       <h1>Explore our menu</h1>
@@ -14,9 +15,9 @@ const ExploreMenu = ({ category, setCategory }) => {
       <div className='explore-menu-list'>
         {menu_list.map((item, index) => {
           return (
-            <div onClick={()=> setCategory(prev=>prev === item.menu_name ? "All" : item.menu_name)}key={index} className='explore-menu-list-item'>
+            <div onClick={()=> setCategory(prev=>prev === item.menu_name ? "All" : item.menu_name)} key={index} className='explore-menu-list-item'>
               <img className={category === item.menu_name ? "active" : ""} src={item.menu_image} alt='item image' />
-              <p>{item.menu_name}</p>
+              <p className={category === item.menu_name ? "select" : ""}>{item.menu_name}</p>
             </div>
           );
         })}
