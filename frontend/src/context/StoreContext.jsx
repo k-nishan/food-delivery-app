f5a7af8643c5bf1a009e1cac2c9a2cf829487e08
nishanthan-personal <nishakanaga0708@gmail.com>
2025-01-22 09:35:42 +0530

    cart functions added

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,19 +1,45 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/assets";
 
-export const StoreContext = createContext(null)
+export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState({});
 
-    const contextValue = {
-        food_list
+  const addToCart = (itemId) => {
+    console.log('Add to cart function')
+    if(!cartItems[itemId]){
+        // if the selected item is not available in the cart
+        setCartItems((prev) => ({...prev, [itemId]:1}))
     }
-    
-    return(
-        <StoreContext.Provider value={contextValue}>
-            {props.children}
-        </StoreContext.Provider>
-    )
-}
+    else {
+        // if selected item is already there in the item; just  increasing the count
+        setCartItems((prev) => ({...prev, [itemId]: prev[itemId]+1}))
+    }
+  }
 
-export default StoreContextProvider
+  const removeFromCart = (itemId) => {
+    // decrease the value by 1
+    setCartItems((prev) => ({...prev, [itemId]:prev[itemId]-1}))
+  }
+
+  const contextValue = {
+    food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart
+  };
+
+  useEffect(() => {
+    console.log(cartItems)
+  }, [cartItems])
+
+  return (
+    <StoreContext.Provider value={contextValue}>
+      {props.children}
+    </StoreContext.Provider>
+  );
+};
+
+export default StoreContextProvider;
