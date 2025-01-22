a25e4f07020a71d537475d4afccd454d6833c626
nishanthan-personal <nishakanaga0708@gmail.com>
2025-01-22 20:02:15 +0530

    mycart items page

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,16 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
 import { Link } from "react-router-dom";
+import { StoreContext } from "../../context/StoreContext";
 
 const navbar = ({ setShowLogin }) => {
   const [menu, setMenu] = useState("home");
+  const {getTotalCartAmount} = useContext(StoreContext)
 
   return (
     <div className='navbar'>
-      <img src={assets.logo} alt='logo' className='logo' />
+      <Link to='/'><img src={assets.logo} alt='logo' className='logo' /></Link>
       <ul className='navbar-menu'>
         <Link
           to='/'
@@ -42,8 +44,8 @@ const navbar = ({ setShowLogin }) => {
       <div className='navbar-right'>
         <img src={assets.search_icon} alt='search-icon' />
         <div className='navbar-bag-icon'>
-          <img src={assets.bag_icon} />
-          <div className='dot'></div>
+          <Link to='/cart'><img src={assets.bag_icon} /></Link>
+          <div className={getTotalCartAmount() === 0 ? "" : "dot"}></div>
         </div>
         <button onClick={() => setShowLogin(true)}>sign in</button>
       </div>
