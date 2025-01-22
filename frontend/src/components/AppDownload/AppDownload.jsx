ae23f30d97cbf4e1eb3789998cfbadb0f355577c
nishanthan-personal <nishakanaga0708@gmail.com>
2025-01-22 10:39:41 +0530

    app download platforms

---
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./AppDownload.css";
+import { assets } from "../../assets/assets";
+
+const AppDownload = () => {
+  return (
+    <div className='app-download' id='app-download'>
+      <p>
+        for better experience download <br /> Tomoto App
+        <div className="app-download-platforms">
+            <img src={assets.play_store} alt="" />
+            <img src={assets.app_store} alt="" />
+        </div>
+      </p>
+    </div>
+  );
+};
+
+export default AppDownload;
